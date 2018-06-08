cc1bbe81bda01c7435611ecb283ab36ccdd3ee7e
Vishnu Narayanan <iamwishnu@gmail.com>
2018-06-08 15:58:03 +0530

    temp commit 3

--- main.go
+++ main.go
@@ -10,42 +10,43 @@ import (
 )
 
 func main() {
+	for {
+		branch := flag.String("b", "master", "branch to use")
+		//action := flag.String("a", "init", "action to perform. Available actions: init,next,prev")
+		flag.Parse()
 
-	branch := flag.String("b", "master", "branch to use")
-	//action := flag.String("a", "init", "action to perform. Available actions: init,next,prev")
-	flag.Parse()
-
-	cmd := exec.Command("git", "checkout", *branch)
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Checking out", *branch)
-
-	var (
-		cmdOut []byte
-		err2    error
-	)
-
-	cmdName := "git"
-	cmdArgs := []string{"log", "--pretty=%h"}
-	if cmdOut, err2 = exec.Command(cmdName, cmdArgs...).Output(); err2 != nil {
-		fmt.Fprintln(os.Stderr, "There was an error running git rev-parse command: ", err2)
-		os.Exit(1)
-	}
-	
-	commits := strings.Split(string(cmdOut),"\n")
-	for i,c := range commits {
-			fmt.Println(i,c)
+		cmd := exec.Command("git", "checkout", *branch)
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
 		}
 
-	fmt.Println("Replaying history...")
-	fmt.Println("Checking out initial commit")
+		fmt.Println("Checking out", *branch)
 
-	cmd = exec.Command("git", "checkout", commits[0])
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
+		var (
+			cmdOut []byte
+			err2   error
+		)
+
+		cmdName := "git"
+		cmdArgs := []string{"log", "--pretty=%h"}
+		if cmdOut, err2 = exec.Command(cmdName, cmdArgs...).Output(); err2 != nil {
+			fmt.Fprintln(os.Stderr, "There was an error running git rev-parse command: ", err2)
+			os.Exit(1)
+		}
+
+		commits := strings.Split(string(cmdOut), "\n")
+		for i, c := range commits {
+			fmt.Println(i, c)
+		}
+
+		fmt.Println("Replaying history...")
+		fmt.Println("Checking out initial commit")
+
+		cmd = exec.Command("git", "checkout", commits[0])
+		err = cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
