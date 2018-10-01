49115eeb52e3f2e5dea83f5f58e56002fb229f43
Vishnu Narayanan <iamwishnu@gmail.com>
2018-10-01 23:35:13 +0530

    get basic functionality working

--- main.go
+++ main.go
@@ -16,46 +16,53 @@ func main() {
 	fmt.Println("Initializing gwalk v0.1.0")
 
 	branch := flag.String("b", "master", "branch to use")
-	// action := flag.String("a", "init", "action to perform. Available actions: init,next,prev")
 	flag.Parse()
+	fmt.Println("Checking out branch ", *branch)
 	cmd := exec.Command("git", "checkout", *branch)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Checking out", *branch)
-
 	history := generateHistory()
 	commits = strings.Split(string(history), "\n")
+	lengthOfCommits := len(commits) - 2
+
+	fmt.Printf("%d commits found ", lengthOfCommits)
 	fmt.Println("Available actions - init,next,prev,exit")
+	fmt.Println(commits[0])
+	i := 0
 
 	for {
-		fmt.Println("===>")
+		fmt.Print(">")
 		reader := bufio.NewReader(os.Stdin)
 		text, _ := reader.ReadString('\n')
-		i := 0
 
-		switch text {
+		switch strings.TrimRight(text, "\n") {
 		case "init":
 			fmt.Println("Checking out initial commit")
 			i = 0
 			checkout(i, commits)
 		case "next":
 			fmt.Println("Moving forward by one commit")
+			if i == lengthOfCommits {
+				fmt.Println("Cannot move forward. Already at the latest commit.")
+				break
+			}
 			i++
 			checkout(i, commits)
 		case "prev":
+			fmt.Println("Moving backward by one commit")
+			if i == 0 {
+				fmt.Println("Cannot move backward. Already at first commit.")
+				break
+			}
 			i--
 			checkout(i, commits)
-			fmt.Println("Moving backward by one commit")
 		case "exit":
 			os.Exit(0)
 		default:
-			fmt.Println("Checking out initial commit")
-			i = 0
-			checkout(i, commits)
-
+			fmt.Println(".....")
 		}
 
 	}
@@ -63,10 +70,12 @@ func main() {
 
 func checkout(i int, commit []string) {
 	cmd := exec.Command("git", "checkout", commits[i])
-	err := cmd.Run()
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(fmt.Sprint(err) + ": " + string(output))
+		os.Exit(1)
 	}
+	return
 }
 
 func generateHistory() []byte {
@@ -77,7 +86,7 @@ func generateHistory() []byte {
 	)
 
 	cmdName := "git"
-	cmdArgs := []string{"log", "--pretty=%h"}
+	cmdArgs := []string{"log", "--reverse", "--pretty=%h"}
 	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
 		fmt.Fprintln(os.Stderr, "There was an error running git log command: ", err)
 		os.Exit(1)
